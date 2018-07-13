caa049666c9de1c868914f3a16f64d727c699dd8
ithuvanian <michd.weaver@gmail.com>
2018-07-13 17:39:36 -0400

    added icons, updated tag view switching

--- Cardid/Content/deckTags.js
+++ Cardid/Content/deckTags.js
@@ -1,17 +1,31 @@
-﻿$(document).ready(function () {
+﻿
+function switchView() {
 
-    $('.tagview-pop').click(function () {
-        $(this).addClass('bold').removeClass('pointer');
+    if ($('.tags-pop').hasClass('hidden')) {
+
+        $('.tagview-pop').addClass('bold').removeClass('pointer');
         $('.tagview-alph').removeClass('bold').addClass('pointer');
         $('.tags-alph').addClass('hidden');
+        $('.toggle-right').addClass('hidden');
         $('.tags-pop').removeClass('hidden');
-    });
+        $('.toggle-left').removeClass('hidden');
 
-    $('.tagview-alph').click(function () {
-        $(this).addClass('bold').removeClass('pointer');
+    } else {
+  
+        $('.tagview-alph').addClass('bold').removeClass('pointer');
         $('.tagview-pop').removeClass('bold').addClass('pointer');
         $('.tags-pop').addClass('hidden');
+        $('.toggle-left').addClass('hidden');
         $('.tags-alph').removeClass('hidden');
+        $('.toggle-right').removeClass('hidden');
+    }
+}
+
+
+$(document).ready(function () {
+
+    $('.tagview-pop, .tagview-alph, .toggle-left, .toggle-right').on('click', function () {
+        switchView();
     });
 
 });
