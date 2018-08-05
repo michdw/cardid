11e65850a86b4516a7d8c74350b68fc16514aa4e
ithuvanian <michd.weaver@gmail.com>
2018-08-04 20:32:32 -0400

    formatting in changeUserInfo view

--- Cardid/Content/editDeck.js
+++ Cardid/Content/editDeck.js
@@ -1,36 +1,49 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
-    //expand and collapse tag view
-    $('.expand').on('click', function () {
-        $(this).addClass('hidden');
-        $('.partial-tags').addClass('hidden');
-        $('.collapse').removeClass('hidden');
-        $('.full-tags').removeClass('hidden');
-    });
+    function switchView() {
 
-    $('.collapse').on('click', function () {
-        $(this).addClass('hidden');
-        $('.partial-tags').removeClass('hidden');
-        $('.full-tags').addClass('hidden');
-        $('.expand').removeClass('hidden');
-    });
+        if ($('.tags-pop').hasClass('hidden')) {
 
-    //show and hide delete option
-    $('.delete-init').on('click', function () {
-        $(this).hide();
-        $('.delete-submit').css('display', 'inline-block');
-    });
+            $('.tagview-pop').addClass('bold').removeClass('pointer');
+            $('.tagview-alph').removeClass('bold').addClass('pointer');
+            $('.tags-alph').addClass('hidden');
+            $('.toggle-right').addClass('hidden');
+            $('.tags-pop').removeClass('hidden');
+            $('.toggle-left').removeClass('hidden');
 
-    $('.cancel-btn').on('click', function () {
-        $('.delete-submit').css('display', 'none');
-        $('.delete-init').show();
-    });
+        } else {
 
-    //show rename option
-    $('.rename-init').on('click', function () {
-        $(this).hide();
-        $('.rename-submit').removeClass('hidden');
+            $('.tagview-alph').addClass('bold').removeClass('pointer');
+            $('.tagview-pop').removeClass('bold').addClass('pointer');
+            $('.tags-pop').addClass('hidden');
+            $('.toggle-left').addClass('hidden');
+            $('.tags-alph').removeClass('hidden');
+            $('.toggle-right').removeClass('hidden');
+        }
+    }
+
+    $(document).ready(function () {
+
+        $('.tagview-pop, .tagview-alph, .toggle-left, .toggle-right').on('click', function () {
+            switchView();
+        });
+
+        //show and hide delete option
+        $('.delete-init').on('click', function () {
+            $(this).hide();
+            $('.delete-submit').css('display', 'inline-block');
+        });
+
+        $('.cancel-btn').on('click', function () {
+            $('.delete-submit').css('display', 'none');
+            $('.delete-init').show();
+        });
+
+        //show rename option
+        $('.rename-init').on('click', function () {
+            $(this).hide();
+            $('.rename-submit').removeClass('hidden');
+        });
     });
 
 });
