243673b6dd661a39877b2672f0945a520046e1b9
ithuvanian <michd.weaver@gmail.com>
2018-08-08 17:14:10 -0400

    limited leaderboards to 10 items on homepage

--- Cardid/Models/Stats.cs
+++ Cardid/Models/Stats.cs
@@ -10,5 +10,44 @@ namespace Cardid.Models
         public Dictionary<string, int> ActiveUsers { get; set; }
         public Dictionary<Deck, int> ActiveDecks { get; set; }
         public Dictionary<Tag, int> PopularTags { get; set; }
+
+        public List<string> ActiveUsersOrder
+        {
+            get
+            {
+                List<string> names = new List<string>();
+                foreach (string name in ActiveUsers.Keys)
+                {
+                    names.Add(name);
+                }
+                return names;
+            }
+        }
+
+        public List<Deck> ActiveDecksOrder
+        {
+            get
+            {
+                List<Deck> names = new List<Deck>();
+                foreach (Deck deck in ActiveDecks.Keys)
+                {
+                    names.Add(deck);
+                }
+                return names;
+            }
+        }
+
+        public List<Tag> PopularTagsOrder
+        {
+            get
+            {
+                List<Tag> tags = new List<Tag>();
+                foreach (Tag tag in PopularTags.Keys)
+                {
+                    tags.Add(tag);
+                }
+                return tags;
+            }
+        }
     }
 }
