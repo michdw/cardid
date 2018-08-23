38ba46db43ca48b8e1836f7c08de99e87155acd3
ithuvanian <michd.weaver@gmail.com>
2018-08-23 14:45:21 -0400

    formatting in search text view

--- Cardid/Content/searchText.js
+++ Cardid/Content/searchText.js
@@ -2,12 +2,12 @@
 $(document).ready(function () {
     $('.show-btn').on('click', function () {
         $(this).hide();
-        //$(this).closest('.results-container').find('.hide-btn').show();
-        $(this).closest('.results-container').find('.results-section').removeClass('hidden');
+        var currentSection = $(this).closest('.results-container').find('.results-section');
+        $('.results-section').not(currentSection).find('.hide-btn').click();
+        currentSection.removeClass('hidden');
     });
 
     $('.hide-btn').on('click', function () {
-        //$(this).hide();
         $(this).closest('.results-container').find('.results-section').addClass('hidden');
         $(this).closest('.results-container').find('.show-btn').show();
     });
