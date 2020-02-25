c4605b33e3bbe299df3a5bb621c2d3bcd84f69be
ithuvanian <michd.weaver@gmail.com>
2020-02-24 21:33:28 -0500

    added unit test project for selenium web driver

---
+++ CardidTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CardidTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CardidTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a59bff5-b78c-4e5a-9b4e-35ea128c0e90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CardidTest/SeleniumWebDriver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace CardidTest
+{
+    [TestClass]
+    public class SeleniumWebDriver
+    {
+        static IWebDriver driverFF;
+        static IWebDriver driverGC;
+
+        [AssemblyInitialize]
+        public static void Setup(TestContext context)
+        {
+            driverFF = new FirefoxDriver();
+            driverGC = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void TestFirefoxDriver()
+        {
+            string url = "https://cardid.apphb.com/";
+            driverFF.Navigate().GoToUrl(url);
+            driverFF.Quit();
+        }
+
+        [TestMethod]
+        public void TestChromeDriver()
+        {
+            string url = "https://cardid.apphb.com/";
+            driverGC.Navigate().GoToUrl(url);
+            driverGC.Quit();
+        }
+
+    }
+}
